2f264621e7d3f7a575e32d262c199112e520c2cc
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-08-02 18:03:05 +0300

    Add files via upload

--- Xbonacci.java
+++ Xbonacci.java
@@ -1,6 +1,20 @@
 import java.util.Arrays;
 
+/**
+ * This program calculate sequance of numbers,similar to fibonacci
+ */
+
 public class Xbonacci {
+    /**
+     * @param signature signature of xbonacci sequence
+     *                  example - sequence {0,1},thats why it
+     *                  will be sequence of numbers like X member
+     *                  equals to sum of two previous members
+     * @param num       its a number of xbonacci sequence's members
+     * @return result array with whole sequence of Xbonacci numbers
+     */
+
+
     public double[] xbonacci(double[] signature, int num) {
 
         double[] result = new double[num];
@@ -18,9 +32,15 @@ public class Xbonacci {
     }
 }
 
+/**
+ * testing xbonacci sequence
+ */
 class Test {
     public static void main(String[] args) {
         Xbonacci xbonacci = new Xbonacci();
-        System.out.println(Arrays.toString(xbonacci.xbonacci(new double[]{0, 1}, 10)));
+        System.out.println(Arrays.toString(
+                xbonacci.xbonacci(new double[]{0, 1}, 10)));
+        System.out.println(Arrays.toString(
+                xbonacci.xbonacci(new double[]{0, 0,0,0,1}, 10)));
     }
 }
